a0b7d00c94788f0ffadee8e1fcac44a8a47e7bb3
Michael Nutt <michael@nuttnet.net>
2026-02-23 14:27:39 -0500

    check in dist/ for dist branch for temporarily importing module via git

---
+++ dist/compiler/capnpc-js.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { c as cliMain } from '../shared/capnp-es.ClefdfxN.mjs';
+import 'node:fs/promises';
+import 'node:path';
+import 'node:child_process';
+import '../shared/capnp-es.B9oGtM6e.mjs';
+import 'typescript';
+import '../capnp/schema.mjs';
+import '../shared/capnp-es.CQedz-ec.mjs';
+import '../shared/capnp-es.DDsOLV-R.mjs';
+import '../shared/capnp-es.D_lMXifH.mjs';
+import 'node:fs';
+
+await cliMain("js");
