0259b5303b35aed384781beef11dca996702914f
Michael Nutt <michael@nuttnet.net>
2026-02-23 14:27:39 -0500

    check in dist/ for dist branch for temporarily importing module via git

---
+++ dist/compiler/index.mjs
@@ -0,0 +1,6 @@
+export { c as compileAll } from '../shared/capnp-es.B9oGtM6e.mjs';
+import 'typescript';
+import '../capnp/schema.mjs';
+import '../shared/capnp-es.CQedz-ec.mjs';
+import '../shared/capnp-es.DDsOLV-R.mjs';
+import '../shared/capnp-es.D_lMXifH.mjs';
